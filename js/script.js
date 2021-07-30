1107e817055f4dfd51e09a086e4fbeddc36d152f
Mishail <60071039+mishail6918@users.noreply.github.com>
2021-07-30 13:27:20 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,13 +3,53 @@ $(document).ready(function(){
         slidesToShow: 1,
         arrows: false,
         asNavFor: ".slider",
-        speed: 500
+        centerPadding: '0px',
+        speed: 500,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    centerPadding: '30px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 425,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
     $('.slider').slick({
         centerMode: true,
-        centerPadding: '60px',
+        centerPadding: '30px',
         slidesToShow: 3,
         asNavFor: ".slider-text",
-        speed: 500
+        speed: 500,
+        responsive: [
+            {
+                breakpoint: 426,
+                settings: {
+                    slidesToShow: 1,
+                    centerPadding: '60px'
+                }
+            },
+            {
+                breakpoint: 376,
+                settings: {
+                    slidesToShow: 1,
+                    centerPadding: '0px',
+                    arrows: false
+                }
+            }
+        ]
     });
+    //burger-menu
+    $('.burger-open').click(function() {
+        $('.mobile-menu').addClass('active-mobile');
+    });
+    $('.close-btn').click(function() {
+        $('.mobile-menu').removeClass('active-mobile');
+    })
 });
